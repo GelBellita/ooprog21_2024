7aa4fc7dcc7aa9e047a332a3a53fc2ea97b1ca8e
EngelBellita <150580402+GelBellita@users.noreply.github.com>
2024-08-14 23:29:49 +0800

    Add files via upload

---
+++ Chapter 1/Triangle.java
@@ -0,0 +1,13 @@
+public class Triangle
+ {
+    public static void main(String[] args) {
+       
+        
+        System.out.println("      T        ");
+        System.out.println("     TTT       ");
+        System.out.println("    TTTTT      ");
+        System.out.println("   TTTTTTTT    ");
+        System.out.println("  TTTTTTTTTTT  ");
+        System.out.println(" TTTTTTTTTTTTT ");
+    }
+}
