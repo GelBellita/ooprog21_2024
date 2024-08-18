c161a38a69f85469b6e09e302f996af26839d07f
EngelBellita <150580402+GelBellita@users.noreply.github.com>
2024-08-18 12:37:27 +0800

    MovieQuoteInfo.java

--- Chapter 1/MovieQuoteInfo.java
+++ Chapter 1/MovieQuoteInfo.java
@@ -1,34 +1,34 @@
 public class MovieQuoteInfo {
     public static void main(String[] args) {
-        // First quote
+       
         System.out.println("\"90% Of Life Is Confidence, And The Thing About Confidence Is That No One Knows If It’s Real Or Not.\"");
         System.out.println("Title: Euphoria");
         System.out.println("Said by: Madeleine");
         System.out.println("Year: 2019");
         System.out.println();
 
-        // Second quote
+        
         System.out.println("\"Plenty Of Great, Intelligent, Funny, Interesting, And Creative People Have Struggled With The Same Things You Struggle With.\"");
         System.out.println("Title: Euphoria");
         System.out.println("Said by: Leslie Bennett");
         System.out.println("Year: 2019");
         System.out.println();
 
-        // Third quote
+        
         System.out.println("\"I will leave tomorrow’s problems to tomorrow’s me.\"");
         System.out.println("Title: One Punch Man");
         System.out.println("Said by: Saitama");
         System.out.println("Year: 2015");
         System.out.println();
 
-        // Fourth quote
+        
         System.out.println("\"Remember, it is the heart that drives people so it can grow as strong as you need it to.\"");
         System.out.println("Title: Demon Slayer");
         System.out.println("Said by: Tanjiro Kamado");
         System.out.println("Year: 2019");
         System.out.println();
 
-        // Fifth quote
+        
         System.out.println("\"When do you think people die? When they are shot through the heart by the bullet of a pistol? No. When they are ravaged by an incurable disease? No... It’s when they're forgotten!\"");
         System.out.println("Title: One Piece");
         System.out.println("Said by: Hiluluk Doctor");
