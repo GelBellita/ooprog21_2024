a9c133e5b0a5310ac6c2f50f2d46e9dbdf0b5121
EngelBellita <150580402+GelBellita@users.noreply.github.com>
2024-09-16 11:42:08 +0800

    Add files via upload

---
+++ Chapter 3/FirstAddressExcercise.java
@@ -0,0 +1,9 @@
+public class FirstAddressExcercise {
+   public static void displayAddress(){
+       System.out.println("OPPO Electronics Corp. \nNo. 18, Haibin Road \nNanshan, Shenzhen \nGuangdong, 518057 \nChina");
+  }
+    public static void main(String[] args) {
+        displayAddress();
+        System.out.println("First Java application");
+    }
+}
